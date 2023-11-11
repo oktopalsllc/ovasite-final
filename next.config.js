e58d8e1737cd1006a686e917a7719f959ab1b413
Michael <gadreelazazel@gmail.com>
2023-11-11 16:53:40 +0100

    Update

--- next.config.js
+++ next.config.js
@@ -7,10 +7,12 @@ const nextConfig = {
         API_URL: process.env.API_URL,
         // POSTGRES_URL: process.env.POSTGRES_URL,
         POSTGRES_PRISMA_URL: process.env.POSTGRES_PRISMA_URL,
+        VERCEL_URL: process.env.VERCEL_URL,
+        STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY
         // POSTGRES_URL_NON_POOLING: process.env.POSTGRES_URL_NON_POOLING,
-        NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
-        CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
-        WEBHOOK_SECRET: process.env.WEBHOOK_SECRET
+        // NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+        // CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+        // WEBHOOK_SECRET: process.env.WEBHOOK_SECRET
     }
 }
 
