417995e6338c506b90bd99f9d7c6e6edec903e44
umahatokula <umahatokula@gmail.com>
2023-11-13 19:50:17 +0100

    update: spinners and landing page fixes

--- src/components/home/content.js
+++ src/components/home/content.js
@@ -6,7 +6,7 @@ import Image from "next/image";
 
 export const Content = () => {
   return (
-    <div className="w-10/12 lg:w-9/12 mx-auto mt-36">
+    <div className="w-10/12 lg:w-9/12 mx-auto md:mt-[750px]">
       <div id="features">
         <div>
           <h1 id="topbtn">Features</h1>
