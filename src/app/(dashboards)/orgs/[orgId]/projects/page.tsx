c3597ed7f4dd5cd7f161189bfda8b314b7682100
umahatokula <umahatokula@gmail.com>
2023-11-15 10:56:25 +0100

    update: project card created

--- src/app/(dashboards)/orgs/[orgId]/projects/page.tsx
+++ src/app/(dashboards)/orgs/[orgId]/projects/page.tsx
@@ -7,25 +7,27 @@ import ProjectCards from "@/components/project/list/Projects";
 
 export default function Projects({ params }: { params: { orgId: string } }) {
   const { orgId } = params;
-  return(
-    <div className="container pt-4">
-    <Separator className="my-6" />
-    <h2 className="text-4xl font-bold col-span-2">Projects</h2>
-    <Separator className="my-6" />
-    <div className="grid gric-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-      <CreateProjectBtn orgId={orgId}/>
-      <Suspense
-        fallback={[1, 2, 3, 4].map((el) => (
-          <ProjectCardSkeleton key={el} />
-        ))}
-      >
-        <ProjectCards orgId={orgId} />
-      </Suspense>
-    </div>
-  </div>
+  return (
+    <>
+      <div className="w-full">
+        <div className="flex items-center justify-between">
+          <h2 className="text-4xl font-bold col-span-2">Projects</h2>
+          <CreateProjectBtn orgId={orgId} />
+        </div>
+
+        <div className="grid gric-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
+          <Suspense
+            fallback={[1, 2, 3, 4].map((el) => (
+              <ProjectCardSkeleton key={el} />
+            ))}
+          >
+            <ProjectCards orgId={orgId} />
+          </Suspense>
+        </div>
+      </div>
+    </>
   );
 }
-
 
 function ProjectCardSkeleton() {
   return <Skeleton className="border-2 border-primary-/20 h-[190px] w-full" />;
