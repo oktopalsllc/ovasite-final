cc7cbb5d3195d4daabaabf61700ebe99e1b9e824
Michael <gadreelazazel@gmail.com>
2023-11-17 08:05:58 +0100

    Added footer to orgs dashboard

--- src/app/(dashboards)/orgs/[orgId]/layout.tsx
+++ src/app/(dashboards)/orgs/[orgId]/layout.tsx
@@ -2,6 +2,7 @@
 import Header from "@/components/orgs/Header";
 import SidebarMenu from "@/components/orgs/SidebarMenu";
 import { SidebarNav } from "@/components/orgs/SidebarNav";
+import Footer from "@/components/orgs/Footer";
 
 export default function OrgsLayout({
   children,
@@ -9,13 +10,19 @@ export default function OrgsLayout({
   children: React.ReactNode;
 }) {
   return (
-    <section className="flex flex-col md:flex-row bg-gray-100 h-full">
-      {/* <SidebarNav activeLink={"projects"} /> */}
-      <SidebarMenu />
-      <section className="w-full">
+    // <section className="flex flex-col md:flex-row bg-gray-100 h-full">
+    //   {/* <SidebarNav activeLink={"projects"} /> */}
+    //   <SidebarMenu />
+    //   <section className="w-full">
+    //     <Header />
+    //     <div className="px-6 md:px-10">{children}</div>
+    //     <Footer />
+    //   </section>
+    // </section>
+    <section className="bg-gray-100 h-full w-full">
         <Header />
         <div className="px-6 md:px-10">{children}</div>
+        <Footer />
       </section>
-    </section>
   );
 }
