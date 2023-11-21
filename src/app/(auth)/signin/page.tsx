a98a32895f22d0ac16e4f6351aab5fe66664bc91
Chidozie Zeno Ezeanekwe <chidozieezeanekwe@gmail.com>
2023-11-21 08:15:42 +0100

    REVAMP

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -1,30 +1,33 @@
-"use client";
-import { authService } from "@/services/auth-service/auth.service.ts";
-import { useRouter } from "next/navigation";
-import { useState } from "react";
-import Link from "next/link";
-import Image from "next/image";
-import "@/styles/styles.css";
-import { yupResolver } from "@hookform/resolvers/yup";
-import * as yup from "yup";
-import { useForm } from "react-hook-form";
-import { useIsMounted } from "@/hooks/useIsMounted";
-import { toast } from "@/components/form/ui/use-toast";
-import { ImSpinner2 } from "react-icons/im";
+'use client';
+import { authService } from '@/services/auth-service/auth.service.ts';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import '@/styles/styles.css';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+import { useForm } from 'react-hook-form';
+import { useIsMounted } from '@/hooks/useIsMounted';
+import { toast } from '@/components/form/ui/use-toast';
+import { ImSpinner2 } from 'react-icons/im';
+import { AiFillEye, AiFillEyeInvisible } from 'react-icons/ai';
 
 const schema = yup.object({
-  email: yup.string().required("Email is required"),
-  password: yup.string().required("Password is required"),
+  email: yup.string().required('Email is required'),
+  password: yup.string().required('Password is required'),
 });
 
 const Page = () => {
   const [isLoading, setIsLoading] = useState(false);
   const router = useRouter();
   const [passwordVisible, setPasswordVisible] = useState(false);
+  const [passwordShown, setPasswordShown] = useState(false);
+  const togglePasswordVisibility = () => setPasswordShown(!passwordShown);
 
-  const togglePasswordVisibility = () => {
-    setPasswordVisible((prev) => !prev);
-  };
+  //   const togglePasswordVisibility = () => {
+  //     setPasswordVisible((prev) => !prev);
+  //   };
 
   const {
     register,
@@ -46,21 +49,21 @@ const Page = () => {
         const access_token = response.data.access_token;
         const userInfo = response.data.userInfo;
 
-        localStorage.setItem("token", access_token);
-        localStorage.setItem("userInfo", JSON.stringify(userInfo));
+        localStorage.setItem('token', access_token);
+        localStorage.setItem('userInfo', JSON.stringify(userInfo));
 
         if (userInfo.organizations.length == 0) {
           setIsLoading(false);
-          router.push("/orgs/create-org");
+          router.push('/orgs/create-org');
         } else {
           setIsLoading(false);
-          router.push("/orgs");
+          router.push('/orgs');
         }
       } else {
         const error = response.error;
         setIsLoading(false);
         toast({
-          title: "Error",
+          title: 'Error',
           description: error,
         });
       }
@@ -96,77 +99,80 @@ const Page = () => {
       <div>
         {/* first page */}
         <div>
-          <div className="grid grid-cols-1 place-content-center relative h-screen">
+          <div className='grid grid-cols-1 place-content-center relative h-screen'>
             {/* Logo */}
-            <div className="top-5 mx-auto ">
-              <Link href="/">
-                <Image
-                  src="/Logo.png"
-                  alt=""
-                  width={70}
-                  height={70}
-                />
+            <div className='top-4 mx-auto '>
+              <Link href='/'>
+                <Image src='/Logo.png' alt='' width={50} height={50} />
               </Link>
             </div>
-
+            <div>
+              <h1 className='title-font text-center font-bold text-xl text-[#001233] mb-4 mt-6'>
+                Welcome Back
+              </h1>
+            </div>
             {/* Login form */}
-            <div className="mx-auto w-full max-w-sm lg:w-96">
-              <div>
-                <h1 className="title-font text-center font-bold text-xl text-[#001233] mb-4 mt-10">
-                  Welcome Back
-                </h1>
-              </div>
-
-              <div className="mt-10">
+            <div className='mx-auto w-full max-w-sm lg:w-96 bg-white bg-opacity-20 backdrop-blur-lg drop-shadow-lg shadow-md rounded p-4'>
+              <div className='mt-10'>
                 <div>
-                  <form onSubmit={handleSubmit(onSubmit)} className="">
+                  <form onSubmit={handleSubmit(onSubmit)} className=''>
                     <div>
                       <label
-                        htmlFor="email"
-                        className="block text-sm font-medium leading-6 text-gray-900"
+                        htmlFor='email'
+                        className='block text-sm font-medium leading-6 text-gray-900'
                       >
                         Email
                       </label>
-                      <div className="mt-2 mb-5">
+                      <div className='mt-2 mb-5'>
                         <input
-                          id="email"
-                          type="email"
-                          autoComplete="email"
+                          id='email'
+                          type='email'
+                          autoComplete='email'
                           required
-                          placeholder="youremail@provider.com"
-                          {...register("email", { required: true })}
+                          placeholder='youremail@provider.com'
+                          {...register('email', { required: true })}
                         />
                       </div>
                     </div>
-                    
+
                     <label
-                        htmlFor="password"
-                        className="text-sm mt-5 font-medium leading-6 text-gray-900"
-                      >
-                        Password
-                      </label>
-                    <div className="mb-5 relative">
+                      htmlFor='password'
+                      className='text-sm mt-5 font-medium leading-6 text-gray-900'
+                    >
+                      Password
+                    </label>
+                    <div className='mb-5 relative'>
                       <input
-                        id="password"
-                        type={passwordVisible ? "text" : "password"}
-                        autoComplete="current-password"
+                        id='password'
+                        type={passwordShown ? 'text' : 'password'}
+                        autoComplete='current-password'
                         required
-                        placeholder="password"
-                        {...register("password", { required: true })}
+                        placeholder='password'
+                        {...register('password', { required: true })}
                       />
-                      <Image
-                        src={passwordVisible ? "/show.png" : "/hide.png"}
+                      {/* <Image
+                        src={passwordVisible ? '/show.png' : '/hide.png'}
                         alt={
-                          passwordVisible ? "Show Password" : "Hide Password"
+                          passwordVisible ? 'Show Password' : 'Hide Password'
                         }
-                        className="absolute top-1/2 transform -translate-y-1/2 right-2 w-5 h-5 cursor-pointer"
+                        className='absolute top-1/2 transform -translate-y-1/2 right-2 w-5 h-5 cursor-pointer'
                         onClick={togglePasswordVisibility}
                         width={20}
                         height={20}
-                      />
+                      /> */}
+                      <span
+                        className='absolute top-1/2 transform -translate-y-1/2 right-2 w-5 h-5 cursor-pointer'
+                        onClick={togglePasswordVisibility}
+                      >
+                        {passwordShown ? (
+                          <AiFillEye className='w-6 h-6 text-[#001233]' />
+                        ) : (
+                          <AiFillEyeInvisible className='w-6 h-6 text-[#001233]' />
+                        )}
+                      </span>
                     </div>
 
-                    <div className="flex items-center justify-between">
+                    <div className='flex items-center justify-between'>
                       {/* <div className="flex items-center">
                         <input
                           id="remember-me"
@@ -182,10 +188,10 @@ const Page = () => {
                         </label>
                       </div> */}
 
-                      <div className="text-sm leading-6 mb-5">
+                      <div className='text-sm leading-6 mb-5'>
                         <Link
-                          href="/ForgotPassword"
-                          className="font-semibold text-[#001233] hover:text-[#FF595A]"
+                          href='/ForgotPassword'
+                          className='font-semibold text-[#001233] hover:text-[#FF595A]'
                         >
                           Forgot password?
                         </Link>
@@ -194,13 +200,13 @@ const Page = () => {
 
                     <div>
                       <button
-                        type="submit"
-                        className="flex items-center w-full justify-center rounded-md bg-[#FF595A] px-3 py-1.5 text-sm font-bold leading-6 text-[white] shadow-sm hover:bg-[#fe5000] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#001233]"
+                        type='submit'
+                        className='flex items-center w-full justify-center rounded-md bg-[#001333] px-3 py-1.5 text-sm font-bold leading-6 text-[white] shadow-sm hover:bg-[#FF595A] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#001233]'
                       >
                         {isLoading ? (
                           <>
-                            Signing In{" "}
-                            <ImSpinner2 className="ml-4 animate-spin" />
+                            Signing In{' '}
+                            <ImSpinner2 className='ml-4 animate-spin' />
                           </>
                         ) : (
                           <>Sign In</>
@@ -210,22 +216,22 @@ const Page = () => {
                   </form>
                 </div>
 
-                <div className="mt-10">
-                  <div className="relative">
+                <div className='mt-10'>
+                  <div className='relative'>
                     <div
-                      className="absolute inset-0 flex items-center"
-                      aria-hidden="true"
+                      className='absolute inset-0 flex items-center'
+                      aria-hidden='true'
                     >
-                      <div className="w-full border-t border-gray-200" />
+                      <div className='w-full border-t border-gray-200' />
                     </div>
                   </div>
 
-                  <div className="mt-6 grid gap-4">
-                    <p className="text-[#001233] text-sm mt-3 text-center">
-                      Don&apos;t have an account?{" "}
+                  <div className='mt-6 grid gap-4'>
+                    <p className='text-[#001233] text-sm mt-3 text-center'>
+                      Don&apos;t have an account?{' '}
                       <Link
-                        href="/signup"
-                        className="font-bold text-[#FF595A] hover:text-[#001233]"
+                        href='/signup'
+                        className='font-bold text-[#FF595A] hover:text-[#001233]'
                       >
                         Sign Up.
                       </Link>
